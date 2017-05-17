79beff23b282022c3826a55248ff563616b8ffcb
Seth <seth19483@gmail.com>
2017-05-17 11:01:42 -0600

    Make test use vcr

--- spec/feature/search_by_zip_spec.rb
+++ spec/feature/search_by_zip_spec.rb
@@ -3,17 +3,19 @@ require 'rails_helper'
 RSpec.feature "from home page" do
   before(:each) do
     VCR.use_cassette("alt_fuel") do
-      response = AltFuelService.new("80203").search_url
+      @response = AltFuelService.new("80203").search_url
     end
   end
 
   scenario "when filling in search form" do
-    visit root_path
-    fill_in "q", with: "80203"
-    click_on "Locate"
-    expect(current_path).to eq search_path
-    expect(page).to have_css(".stations")
-    expect(page).to have_css('.closest_stations', count: 10)
+    VCR.use_cassette("alt_fuel") do
+      visit root_path
+      fill_in "q", with: "80203"
+      click_on "Locate"
+      expect(current_path).to eq search_path
+      expect(page).to have_css(".stations")
+      expect(page).to have_css('.closest_stations', count: 10)
+    end
   end
 end
 
