e0a524c7af9a02821d90ef8874fa78e0f16071b6
Seth <seth19483@gmail.com>
2017-05-17 10:14:53 -0600

    Trying to convert site to son

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @request = HTTParty.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=#{ENV["auth_key"]}")
+    @request = AltFuelService.search_url(params[:q])
+    binding.pry
   end
-
-
 end
