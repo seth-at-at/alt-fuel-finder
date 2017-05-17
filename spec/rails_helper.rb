7b89b43e74d8adbde5af0a8d930cff9169036708
Seth <seth19483@gmail.com>
2017-05-17 11:01:19 -0600

    Fix location of fixture

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'vcr'
 require 'webmock/rspec'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
 # Add additional requires below this line. Rails is not loaded until this point!
