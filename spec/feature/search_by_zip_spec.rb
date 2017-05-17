83997be9fb363d72387226dd1206023f79b319b4
Seth <seth19483@gmail.com>
2017-05-17 09:32:47 -0600

    Added feature test, ran rake db:migrate

---
+++ spec/feature/search_by_zip_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "from home page" do
+  scenario "when filling in search form" do
+    visit root_path
+    fill_in "q", with: "80203"
+    click_on "Locate"
+    request = HTTParty.
+    expect(request).to eq
+  end
+end
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
